63d48caa3fece15d8079c7c94602ee1360139c54
Chelsea King <ckingarts@gmail.com>
2016-10-25 21:33:31 -0500

    beer song added

--- javascript/hello-world/hello-world.spec.js
+++ javascript/hello-world/hello-world.spec.js
@@ -3,11 +3,11 @@ var HelloWorld = require('./hello-world');
 describe('Hello World', function() {
   var helloWorld = new HelloWorld();
 
-  xit('says hello world with no name', function() {
+  it('says hello world with no name', function() {
     expect(helloWorld.hello('')).toEqual('Hello, World!');
   });
 
-  xit('says hello to bob', function() {
+  it('says hello to bob', function() {
     expect(helloWorld.hello('Bob')).toEqual('Hello, Bob!');
   })
 
